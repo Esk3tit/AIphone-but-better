eecbf3571d304524f5ab9ed30b6bed29b816f4d4
Kai Pan <khaiphn41@gmail.com>
2022-11-08 22:48:13 -0800

    Removed files from previous location

--- config.py
+++
@@ -1,6 +0,0 @@
-# Location of the stable-diffusion-docker directory
-STABLE_DIFFUSION_PATH = '/home/allen/Data/stable-diffusion-docker'
-# Location where generated images should be stored
-DATA_PATH = 'data'
-# Secret key
-FLASK_SECRET_KEY = 'super secret key'
--- static/index.js
+++
@@ -1,5 +0,0 @@
-function pop(asdf) {
-    var b = document.getElementById('imagepreview');
-    b.src = document.getElementById(asdf).src;
-    $('#imagemodal').modal('show');
-}

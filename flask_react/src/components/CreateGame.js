d37ce1a42cba471f0ed955b4fcd6f8f3938f2396
Kai Pan <khaiphn41@gmail.com>
2022-11-08 23:37:33 -0800

    Updated forms for login

--- flask_react/src/components/CreateGame.js
+++ flask_react/src/components/CreateGame.js
@@ -1,18 +1,39 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function CreateGame() {
     return (
         <div id="new_game_container">
-            <form action="/create_game" method="get">
-            <legend>OR CREATE A NEW GAME</legend>
-            <fieldset>
-                <div class="form-group">
-                <label for="num_turns" class="form-label mt-4">Number of Turns</label>
-                <input type="text" class="form-control" id="num_turns" name="num_turns" placeholder="Enter number of turns" />
+            {/* <form action="/create_game" method="get">
+                <fieldset>
+                    <div class="form-group">
+                    <label for="num_turns" class="form-label mt-4">Number of Turns</label>
+                    <input type="text" class="form-control" id="num_turns" name="num_turns" placeholder="Enter number of turns" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">New Game</button>
+                </fieldset>
+            </form> */}
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <div>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Number of Turns"
+                        placeholder="Enter number of turns"
+                    />
                 </div>
-                <button type="submit" class="btn btn-primary">New Game</button>
-            </fieldset>
-            </form>
+                <Button variant="contained">New Game</Button>
+            </Box>
         </div>
     );
 }
--- flask_react/src/components/JoinGame.js
+++ flask_react/src/components/JoinGame.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function JoinGame() {
   // <input type="text" class="form-control" id="game_id" name="game_id" placeholder="Enter game id" {% if game_id %}value="{{game_id}}"{% endif %} />
-  // Fix above on line 17 for react
+  // Fix above on line 17 for react conditional attribute
   return (
       <div id="login_container">
-        <form action="/login" method="get">
+        {/* <form action="/login" method="get">
           <fieldset>
             <div class="form-group">
               <label for="username" class="form-label mt-4">Username</label>
@@ -17,7 +21,31 @@ export default function JoinGame() {
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </fieldset>
-        </form>
+        </form> */}
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              placeholder="Enter username"
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Game ID"
+              placeholder="Enter game id"
+            />
+          </div>
+          <Button variant="contained">Submit</Button>
+        </Box>
       </div>
   );
 }

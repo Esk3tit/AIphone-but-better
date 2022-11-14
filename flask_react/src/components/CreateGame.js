1ad6d08edfc4d3871d56e17fc8286c348a7c4e15
Kai Pan <khaiphn41@gmail.com>
2022-11-13 19:04:57 -0800

    Fixed the flask backend interaction by making the backend an API, added toasts and alerts for generating game id but we need to pull up state so that it can be filled in the join game component.

--- flask_react/src/components/CreateGame.js
+++ flask_react/src/components/CreateGame.js
@@ -3,47 +3,57 @@ import React, { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 import axios from 'axios';
+
 
 export default function CreateGame() {
 
     const [numTurns, setNumTurns] = useState("");
+    const [openAlert, setOpenAlert] = useState(false);
+    const [gameId, setGameId] = useState("");
 
     async function redirectToGame(e) {
         e.preventDefault();
-        //const data = await axios.get("/create_game", { params: { num_turns: numTurns } });
-        const data = await fetch("http://localhost:5000/create_game", { method: "GET", params: { num_turns: numTurns } });
-        console.log(data);
-        const newUrl = data.data.url;
-        
-        window.location = newUrl;
+        const res = await axios.get("/create_game", { params: { num_turns: numTurns } });
+        //const data = await fetch("http://localhost:5000/create_game", { method: "GET", params: { num_turns: numTurns } });
+        console.log(res.data.game_id);
+        setGameId(res.data.game_id);
+        const newUrl = "/?game_id=" + res.data.game_id;
+        window.history.replaceState(null, "", newUrl);
+        setOpenAlert(true);
+    }
+
+    const handleClose = () => {
+        setOpenAlert(false);
     }
 
     return (
-        <form onSubmit={redirectToGame} id="new_game_container">
-            {/* <form action="/create_game" method="get">
-                <fieldset>
-                    <div class="form-group">
-                    <label for="num_turns" class="form-label mt-4">Number of Turns</label>
-                    <input type="text" class="form-control" id="num_turns" name="num_turns" placeholder="Enter number of turns" />
-                    </div>
-                    <button type="submit" class="btn btn-primary">New Game</button>
-                </fieldset>
-            </form> */}
-            <Grid2 container sx={{ m: '3px' }} rowSpacing={3} direction="column" alignItems="center" justifyContent="center">
-                <Grid2 item>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Number of Turns"
-                        placeholder="Enter number of turns"
-                        onChange={e => setNumTurns(e.target.value)}
-                        value={numTurns}
-                    />
+        <div>
+            <form onSubmit={redirectToGame} id="new_game_container">
+                <Grid2 container sx={{ m: '3px' }} rowSpacing={3} direction="column" alignItems="center" justifyContent="center">
+                    <Grid2 item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Number of Turns"
+                            placeholder="Enter number of turns"
+                            onChange={e => setNumTurns(e.target.value)}
+                            value={numTurns}
+                            name="num_turns"
+                        />
+                    </Grid2>
+                    <Button variant="contained" type="submit">New Game</Button>
                 </Grid2>
-                <Button variant="contained" type="submit">New Game</Button>
-            </Grid2>
-        </form>
+            </form>
+            <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="info" sx={{ width: '100%' }}>
+                    The game ID to share with your friends is: {gameId}
+                </Alert>
+            </Snackbar>
+        </div>
+        
     );
 }

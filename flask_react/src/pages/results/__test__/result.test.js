23953ba37cbb424b96718f2485b7456a5a34197f
Khai Phan <khaiphn41@gmail.com>
2023-01-12 04:56:11 -0800

    Fixed test errors with ECMAscript imports, and added some tests for the results page with mocked location state to display data in results.

---
+++ flask_react/src/pages/results/__test__/result.test.js
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react';
+import Results from '../results';
+import { MemoryRouter } from 'react-router-dom';
+
+it('should render the correct username field', () => {
+
+  render(
+    <MemoryRouter initialEntries={[{
+        state: {
+          username: "test",
+          game_id: 1,
+          round_number: 3,
+          player_rounds_list: [{username: "test", rounds: [{image_id: 1, prompt: "test prompt"}]}]
+        }
+      }]}
+    >
+      <Results />
+    </MemoryRouter>
+  );
+  const usernameText = screen.getByText(/Username: test/i);
+  expect(usernameText).toBeInTheDocument();
+
+});
+
+it('should render the correct game id field', () => {
+
+  render(
+    <MemoryRouter initialEntries={[{
+        state: {
+          username: "test",
+          game_id: 1,
+          round_number: 3,
+          player_rounds_list: [{username: "test", rounds: [{image_id: 1, prompt: "test prompt"}]}]
+        }
+      }]}
+    >
+      <Results />
+    </MemoryRouter>
+  );
+  const gameidText = screen.getByText(/Game id: 1/i);
+  expect(gameidText).toBeInTheDocument();
+
+});
+
+it('should render the correct round number field', () => {
+
+  render(
+    <MemoryRouter initialEntries={[{
+        state: {
+          username: "test",
+          game_id: 1,
+          round_number: 3,
+          player_rounds_list: [{username: "test", rounds: [{image_id: 1, prompt: "test prompt"}]}]
+        }
+      }]}
+    >
+      <Results />
+    </MemoryRouter>
+  );
+  const roundNumberText = screen.getByText(/Round number: 3/i);
+  expect(roundNumberText).toBeInTheDocument();
+
+});
